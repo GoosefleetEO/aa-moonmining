d327b620e5025e5e81e4a90bfd345a21e3b3b8d3
Unknown <rhartnett35@gmail.com>
2018-10-29 05:32:12 -0400

    Mainly Models
    Also, add auth_hooks and urls

---
+++ moonstuff/auth_hooks.py
@@ -0,0 +1,15 @@
+from . import urls
+from django.utils.translation import ugettext_lazy as _
+from allianceauth import hooks
+from allianceauth.services.hooks import MenuItemHook, UrlHook
+
+
+@hooks.register('menu_item_hook')
+def register_menu():
+    return MenuItemHook(_('Moon Tools'), 'fa fa-moon fa-fw', 'moonstuff:moons_index',
+                        navactive=['moonstuff:'])
+
+
+@hooks.register('url_hook')
+def register_url():
+    return UrlHook(urls, 'moonstuff', r'^moons/')
--- moonstuff/models.py
+++ moonstuff/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class Resource(models.Model):
+    ore = models.TextField(max_length=75)
+    ore_id = models.IntegerField()
+    amount = models.DecimalField(max_digits=11, decimal_places=10)
+
+
+class Moon(models.Model):
+    system_id = models.TextField(max_length=8)
+    moon_id = models.IntegerField()
+    resources = models.ManyToManyField(Resource)
+
+
+def get_fallback_moon():
+    return Moon.objects.get_or_create(system_id=30000142, moon_id=40009087)
+
+
+class Refinery(models.Model):
+    name = models.TextField(max_length=150)
+    size = models.BooleanField()
+    location = models.ForeignKey(Moon, on_delete=models.SET(get_fallback_moon))
+
+    @property
+    def size_to_string(self):
+        if self.size is True:
+            return "Large"
+        else:
+            return "Medium"
