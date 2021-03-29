0f7933073e7b1487a3cc75a706cd2e0f8be42ec2
ErikKalkoken <erik.kalkoken@gmail.com>
2021-03-30 00:24:54 +0200

    Improve load types command

--- moonmining/management/commands/moonmining_load_types.py
+++ moonmining/management/commands/moonmining_load_types.py
@@ -1,50 +1,40 @@
-import logging
-
 from django.core.management.base import BaseCommand
-from eveuniverse.models import EveCategory
+from eveuniverse.models import EveType
+from eveuniverse.tasks import update_or_create_eve_object
 
+from allianceauth.services.hooks import get_extension_logger
 from app_utils.logging import LoggerAddTag
 
-from moonmining.models import EveOreType
-
 from ... import __title__, constants
+from ...models import EveOreType
 from . import get_input
 
-logger = LoggerAddTag(logging.getLogger(__name__), __title__)
+logger = LoggerAddTag(get_extension_logger(__name__), __title__)
 
 
 class Command(BaseCommand):
     help = "Preload all necessary types."
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--force-reload",
-            action="store_true",
-            help="Force reloading of all objects from ESI",
-        )
-
     def handle(self, *args, **options):
-        self.stdout.write("Preloading types for Moon Mining")
+        self.stdout.write("Loading all ore types from ESI. This can take a while.")
+        ore_types_count = EveOreType.objects.count()
+        self.stdout.write(
+            f"You currently have {ore_types_count} ore types in your database."
+        )
         self.stdout.write()
         user_input = get_input("Are you sure you want to proceed? (y/N)?")
 
         if user_input.lower() == "y":
-            self.stdout.write("Loading ore category incl. related objects...")
-            EveCategory.objects.get_or_create_esi(
-                id=constants.EVE_CATEGORY_ID_ASTEROID, include_children=True
+            self.stdout.write("Tasks for loading ore types have been started.")
+            update_or_create_eve_object.delay(
+                model_name="EveCategory",
+                id=constants.EVE_CATEGORY_ID_ASTEROID,
+                include_children=True,
+                enabled_sections=[
+                    EveType.Section.DOGMAS,
+                    EveType.Section.TYPE_MATERIALS,
+                ],
             )
-            ore_type_ids = EveOreType.objects.filter(
-                eve_group__eve_category_id=constants.EVE_CATEGORY_ID_ASTEROID
-            ).values_list("id", flat=True)
-            self.stdout.write(f"Loading {len(ore_type_ids)} ore types...")
-            if not options["force_reload"]:
-                for ore_type_id in ore_type_ids:
-                    EveOreType.objects.get_or_create_esi(id=ore_type_id)
-            else:
-                self.stdout.write("Forced reload activated...")
-                for ore_type_id in ore_type_ids:
-                    EveOreType.objects.update_or_create_esi(id=ore_type_id)
-
             self.stdout.write(self.style.SUCCESS("Done"))
         else:
             self.stdout.write(self.style.WARNING("Aborted"))
