b5b76adb0848cb70d388aaccfdc6a5ef9db9136b
Peter Pfeufer <info@ppfeufer.de>
2023-02-16 08:05:48 +0000

    Update drop_tables.sql
    Missing table added

--- moonmining/tools/drop_tables.sql
+++ moonmining/tools/drop_tables.sql
@@ -2,6 +2,7 @@
 SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS moonmining_extraction;
 DROP TABLE IF EXISTS moonmining_extractionproduct;
+DROP TABLE IF EXISTS moonmining_miningledgerrecord;
 DROP TABLE IF EXISTS moonmining_moon;
 DROP TABLE IF EXISTS moonmining_moonproduct;
 DROP TABLE IF EXISTS moonmining_notification;
